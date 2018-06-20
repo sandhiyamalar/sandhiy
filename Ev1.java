7b70844a45c646278ae6afdbedfe38503d4ea2d1
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-06-20 09:29:56 +0530

    Create Ev1.java

---
+++ Ev1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Ev1{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
